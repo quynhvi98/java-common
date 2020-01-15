ed7be33e5fb1a8b1170240503d05cc4104ad4fbf
quynh.vi <vi.quynh.31598@gmail.com>
2020-01-15 11:57:16 +0700

    jwt auth

---
+++ src/main/java/com/tqt/javacommon/utils/JWTAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package com.tqt.javacommon.utils;
+
+import java.io.IOException;
+
+/**
+ * Posted from Jan 15, 2020, 11:55 AM
+ *
+ * @author Vi Quynh (vi.quynh.31598@gmail.com)
+ */
+@Compo
+public class JWTAuthenticationFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest req,
+                                    HttpServletResponse res,
+                                    FilterChain chain) throws IOException, ServletException {
+        Authentication authentication = getAuthentication(req);
+        if (null != authentication) {
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            chain.doFilter(req, res);
+        } else {
+            if (req.getRequestURI().indexOf("/swagger-ui.html") >= 0
+                    || req.getRequestURI().indexOf("/webjars") >= 0
+                    || req.getRequestURI().indexOf("/swagger-resources") >= 0
+                    || req.getRequestURI().indexOf("/v2/api-docs") >= 0
+            ) {
+                res.setStatus(HttpStatus.OK.value());
+                chain.doFilter(req, res);
+            } else {
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                res.setStatus(HttpStatus.BAD_REQUEST.value());
+                chain.doFilter(req, res);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/tqt/javacommon/utils/exception/JavaNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tqt.javacommon.utils.exception;
+
+/**
+ * Posted from Jan 15, 2020, 11:52 AM
+ *
+ * @author Vi Quynh (vi.quynh.31598@gmail.com)
+ */
+public class JavaNotFoundException {
+}
